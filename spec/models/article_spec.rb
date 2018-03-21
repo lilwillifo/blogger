e4efed4063c036f201b94ee4fd56fa1c79c1cce3
lilwillifo <margaretwilliford4@gmail.com>
2018-03-21 13:43:32 -0600

    add article model

--- spec/models/article_spec.rb
+++ spec/models/article_spec.rb
@@ -2,7 +2,7 @@ require 'rails_helper'
 
 describe Article, type: :model do
   describe 'validations' do
-    it {should validate_precense_of(:title)}
-    it {should validate_precense_of(:body)}
+    it {should validate_presence_of(:title)}
+    it {should validate_presence_of(:body)}
   end
 end
